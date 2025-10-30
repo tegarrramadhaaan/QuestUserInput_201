c70d1a57cd67e7b23ddd101577563c5f546bb9d6
tegarrramadhaaan <anakrama2000@gmail.com>
2025-10-30 13:07:16 +0700

    menambahkan color

--- app/src/main/java/com/example/pertemuan4/ui/theme/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan4/ui/theme/TeksEdit.kt
@@ -1,95 +1,125 @@
-package com.example.pertemuan4.ui.theme
+package com.example.pertemuan4
 
-import android.R
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun FormatDataDiri(modifier: Modifier
-){
-    var textNama by remember { mutableStateOf(value = "") }
-    var textAlamat by remember {mutableStateOf(value = "")}
-    var textJK by remember { mutableStateOf(value = "")}
+fun FormDataDiri(modifier: Modifier) {
 
-    //variable-variable untuk menyimpan data yang di peroleh
-    var nama by remember { mutableStateOf(value = "") }
-    var alamat by remember { mutableStateOf(value = "") }
-    var jenis by remember { mutableStateOf(value = "") }
+    // variabel-variabel untuk mengingat nilai masukan dari keyboard
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
 
-    val gender:List<string> = listOf("laki-laki","perempuan")
+    // variabel-variabel untuk menyimpan data diperoleh dari komponen
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenis by remember { mutableStateOf("") }
 
-    column(modifier = Modifier.padding(top = 50dp),
-        verticalArrrangment = Arrangement.top,
-        horizontalAligment = aligment.CenterHorizontally) {
+    val gender: List<String> = listOf("Laki-laki", "Perempuan")
+
+    Column(
+        modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         OutlinedTextField(
             value = textNama,
-            singleLine = true
+            singleLine = true,
             shape = MaterialTheme.shapes.large,
-            modifier = Modifier.width(width= 250.dp),
-            label = {Text(text = "Nama Lengkap")},
+            modifier = Modifier.width(250.dp),
+            label = { Text(text = "Nama Lengkap") },
             onValueChange = {
                 textNama = it
             }
         )
         Row {
             gender.forEach { item ->
-                Row { modifier = Modifier.selectable(
-                    selected = textJK == item,
-                    onClick =  { textJK = item}
-                ) }
-                Text(text = item)
+                Row(
+                    modifier = Modifier.selectable(
+                        selected = textJK == item,
+                        onClick = {
+                            textJK = item
+                        }
+                    )
+                ) {
+                    Text(item)
+                }
             }
         }
     }
+
     OutlinedTextField(
         value = textAlamat,
         singleLine = true,
-        modifier = Modifier.width(width= 250.dp),
-        label = { Text(text = "Alamat lengkap")},
+        modifier = Modifier.width(250.dp),
+        label = {Text(text = "Alamat Lengkap")},
         onValueChange = {
-            textAlamat = it }
+            textAlamat = it
+        }
     )
+
     HorizontalDivider(
-        modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_medium),
-            top = dimensionResource(id = R.dimen.padding_small),
-            thickness = dimensionResource(id = R.dimen.divider_tipis ))
-        color = Color.Darkgrey
+        modifier = Modifier.padding(
+            bottom = dimensionResource(R.dimen.padding_medium),
+            top = dimensionResource(R.dimen.padding_medium
+            )),
+        thickness = dimensionResource(R.dimen.divider_tipis),
+        color = Color.DarkGray
+    )
+    Button(
+        modifier = Modifier.fillMaxWidth(1f),
+        // the button is enabled when the user makes a selection
+        enabled = textAlamat.isNotEmpty(),
+        onClick = {
+            nama=textNama
+            jenis=textJK
+            alamat=textAlamat
+        }
+    ){
+        Text(text = stringResource(R.string.submit))
+    }
 
+    HorizontalDivider(
+        modifier = Modifier.padding(
+            bottom = dimensionResource(R.dimen.padding_medium),
+            top = dimensionResource(
+                id = R.dimen.padding_medium
+            )),
+        thickness = dimensionResource(R.dimen.divider_tipis),
+        color = Color.DarkGray
     )
 
-    Button(
-        modifier = Modifier.fillMaxWidth(fraction = if),
-        enable = textAlamat.isNotEmpty(),
-        onClick = {
-            nama = textNama
-            alamat = textAlamat
-            jenis = textJK
+    ElevatedCard(
+        elevation = CardDefaults.cardElevation(10.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.Black),
+        modifier = Modifier
+            .height(100.dp)
+            .width(300.dp)
+    ){
+        Column(modifier = Modifier.padding(horizontal = 5.dp, vertical = 15.dp),){
 
         }
-
-    )
-
-
-
-
-
-
-
-
-
+    }
 }
