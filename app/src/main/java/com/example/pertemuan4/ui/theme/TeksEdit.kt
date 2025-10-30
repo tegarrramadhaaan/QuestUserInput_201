3db8fb7651ecf6e1cbce66161e5f268724af00e2
tegarrramadhaaan <anakrama2000@gmail.com>
2025-10-30 09:11:47 +0700

    penambahan code

---
+++ app/src/main/java/com/example/pertemuan4/ui/theme/TeksEdit.kt
@@ -0,0 +1,41 @@
+package com.example.pertemuan4.ui.theme
+
+import android.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.LineHeightStyle
+
+@Composable
+fun FormatDataDiri(modifier: Modifier
+){
+    var textNama by remember { mutableStateOf(value = "") }
+    var textAlamat by remember {mutableStateOf(value = "")}
+    var textJK by remember { mutableStateOf(value = "")}
+
+    //variable-variable untuk menyimpan data yang di peroleh
+    var nama by remember { mutableStateOf(value = "") }
+    var alamat by remember { mutableStateOf(value = "") }
+    var jenis by remember { mutableStateOf(value = "") }
+
+    val gender:List<string> = listOf("laki-laki","perempuan")
+
+    column(modifier = Modifier.padding(top = 50dp),
+        verticalArrrangment = Arrangement.top,
+        horizontalAligment = aligment.CenterHorizontally) {
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(width= 250.dp),
+            label = {Text(text = "Nama Lengkap")},
+            onValueChange = {
+                textNama = it
+            }
+        )
+    }
+}
