ad59f967e04dfdf97ce106e6e699a6558482c4d3
tegarrramadhaaan <anakrama2000@gmail.com>
2025-10-30 13:08:10 +0700

    menambahkan color

--- app/src/main/java/com/example/pertemuan4/ui/theme/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan4/ui/theme/TeksEdit.kt
@@ -121,7 +121,7 @@ fun FormDataDiri(modifier: Modifier) {
         Column(modifier = Modifier.padding(horizontal = 5.dp, vertical = 15.dp),){
             Text(text = "Nama :"+nama, color = Color.White)
             Text(text = "Gender :"+jenis, color = Color.White)
-
+            Text(text = "Alamat :"+alamat, color = Color.White)
         }
     }
 }
